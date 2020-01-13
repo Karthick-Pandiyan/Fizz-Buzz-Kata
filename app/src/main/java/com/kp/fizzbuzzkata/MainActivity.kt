8419f6455d2be41997afd92d9bc8d098b5d7fc97
karthickpandiyan <pandiyan289@gmail.com>
2020-01-13 17:18:15 +0530

    Code cleanup and remove unused libraries.

--- app/src/main/java/com/kp/fizzbuzzkata/MainActivity.kt
+++ app/src/main/java/com/kp/fizzbuzzkata/MainActivity.kt
@@ -1,12 +1,7 @@
 package com.kp.fizzbuzzkata
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
